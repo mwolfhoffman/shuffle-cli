b8e0ce20cc453d65e63ed92fff9c5221134604d1
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2021-03-06 08:45:18 -0700

    shuffling and chunking team members

---
+++ README.md
@@ -0,0 +1,24 @@
+# Python Shuffle CLI
+
+Shuffle allows users to create teams/groups and then shuffle their members randomly. This can be used in schools and other settings for creating random teams for group work. 
+
+I orignally built this as a vue.js, node.js, mongoDB app in 2016 for a code school in Boise, Idaho to use. 
+
+This project is a very basic CLI to practice some of the Python commands I will need in my re-write for Shuffle. I am planning to write a new serverless version of that app using AWS Cognito, Lambda, DynamoDB, EC2, and other features to learn more about cloud architecture and AWS offerings.
+
+This is a very basic CLI so not all CRUD operations that exist in the full Shuffle app will appear here. 
+
+## Features
+
+1. Create and Read Teams.
+2. Add members to a team.
+3. Shuffle teams and split them into groups of differeing sizes.
+
+## Technology Used:
+
+Python
+
+## To Run:
+1. Python 3.x is required
+2. git clone project
+3. `python main.py`
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from classes.team import team
 from classes.member import member
+import random
 
 teams = []
 
 def check_if_team_exists_by_name(name):
-    if len(teams)==0:
-        return False
-    else:
-        for team in teams:
-            if(team.name == name):
-                return True
+    if name is None:
+        raise "You must provide a team name"
+    try:
+        if len(teams)==0:
+            return False
+        else:
+            for team in teams:
+                if(team.name == name):
+                    return True
+    except:
+        print("An error must have occurred trying to find that team")
     return False
 
 def get_team_by_name(name):
     for team in teams:
+        print("\n iterating through team", team.name)
         if(team.name == name):
+            print("Found the team! ", team.name)
             return team
+    return None
 
 
 def init_create_team():
@@ -31,14 +40,13 @@ def init_create_team():
     print("You now have ", len(teams), "teams")
     main()
 
-
-
 def init_add_member():
     team_name = input("What team would you like to add this new member to?")
 
     if check_if_team_exists_by_name(team_name):
         member_name = input("What would you like to name your new team member?")
         team = get_team_by_name(team_name)
+        print('\nthe check if team exists function returned ', team)
         new_member = member(member_name)
         team.members.append(new_member)
         print(new_member.name, " has been added to ", team.name)
@@ -49,9 +57,74 @@ def init_add_member():
         main()
 
 
+def print_each_member_in_team(name):
+    team = get_team_by_name(name)
+    try:
+        for member in team.members:
+            print(member.name)
+    except:
+        print("Sorry that team doesn't seem to exist")
+
+
+def chunk_team_members(team, num):
+    print("Chunking team ", team, " into ", num, " chunks...")
+    list_of_chunks =[]
+    start_chunk = 0
+    end_chunk = start_chunk+num
+    while end_chunk <= len(team.members)+num:
+        chunk_ls = team.members[start_chunk: end_chunk]
+        list_of_chunks.append(chunk_ls)
+        start_chunk = start_chunk +num
+        end_chunk = end_chunk+num    
+    
+    return list_of_chunks
+    print("Chunking is complete.")
+    print("\n LIST OF CHUNKS: ", list_of_chunks)
 
 def init_shuffle_team():
-    print("Not implemented yet")
+    team_name = input("What team would you like to shuffle?")
+    print_each_member_in_team(team_name)
+    try:
+        team = get_team_by_name(team_name)
+        print("About to shuffle ", team)
+        number_of_chunks = input("How many people per each team would you like?")
+        random.shuffle(team.members)
+        
+        chunk_team_members(team, number_of_chunks)
+
+        print(team_name, " has been shuffled and chunked")
+        print_each_member_in_team(team_name)
+    except:
+        print("Error occured attempting to shuffle team")
+
+
+
+def test():
+     # setup teams
+    print("testing...")
+    team_1 = team("team 1")
+    team_2 = team("team 2")
+    team_3 = team("team 3")
+
+    # add members to team 1
+    team_1.members = [member("a"), member("b"), member("c"), member("d"), member("e"), member("f"), member("g"), member("h"), member("i"), member("j")]
+
+    teams.append(team_1)
+    teams.append(team_2)
+    teams.append(team_3)
+
+    print("There are ", len(teams), "teams ")
+
+    # Check team 1 initially
+    print_each_member_in_team("team 1")
+
+    random.shuffle(team_1.members)
+
+    chunk_team_members(team_1, 3)
+
+    print_each_member_in_team(team_1.name)
+
+
 
 
 def print_pretty_headers():
@@ -60,15 +133,16 @@ def print_pretty_headers():
     print('///////////////////////////////////////')
 
 
-
 def main():
-    action = Number = input("What would you like to do?\n\n 1: Create a Team \n\n 2: Add A Member To a Team\n\n 3: Shuffle a Team?")
+    action = Number = input("What would you like to do?\n\n 1: Create a Team \n\n 2: Add A Member To a Team\n\n 3: Shuffle a Team? \n\n 4: To test a sample shuffle and chunk")
     if action == "1":
         init_create_team()
     elif action == "2":
         init_add_member()
     elif action =="3":
         init_shuffle_team()
+    elif action == "4":
+        test()
     else:
         print("")
         print("Sorry! That's not an option. Please Try again. ")
